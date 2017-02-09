da2d142f65fc69c7d2156d7a66906945a202ce80
kwagdaeyong <dayong7387@hanmail.net>
2017-02-09 22:26:17 +0900

    requestBody

--- src/main/java/net/remind/Controller/MessageController.java
+++ src/main/java/net/remind/Controller/MessageController.java
@@ -1,11 +1,12 @@
 package net.remind.Controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +14,17 @@ import net.remind.Service.Answer;
 
 @RestController
 public class MessageController {
+	
 	@RequestMapping("/message")
-	public @ResponseBody Map<String, Object> message( @RequestParam Map<String,Object> getMessage) throws ParseException{
+	public @ResponseBody Map<String, Object> message( @RequestBody List<Map<String,Object>> getMessage) throws ParseException{
 		System.out.println("메세지 받았습니다!");
 		Map <String, Object> sendMessage = new HashMap<String, Object>();
 		Map <String, Object> sendObject = new HashMap<String, Object>();
 		System.out.println("getMessage : " + getMessage);
 		
-		
-		String user_key = getMessage.get("user_key").toString();
-		String type = getMessage.get("type").toString();
-		String content = getMessage.get("content").toString();
+		String user_key = getMessage.get(0).toString();
+		String type = getMessage.get(1).toString();
+		String content = getMessage.get(2).toString();
 		System.out.println("user_key : "+user_key);
 		System.out.println("type : "+type);
 		System.out.println("content : "+content);
