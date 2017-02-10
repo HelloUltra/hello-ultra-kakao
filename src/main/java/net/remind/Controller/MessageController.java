0218c290621c232c07b652bc67e9f428502e9bdb
kwagdaeyong <dayong7387@hanmail.net>
2017-02-10 14:52:51 +0900

    fun

--- src/main/java/net/remind/Controller/MessageController.java
+++ src/main/java/net/remind/Controller/MessageController.java
@@ -1,7 +1,6 @@
 package net.remind.Controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.json.simple.parser.ParseException;
--- src/main/java/net/remind/Service/Answer.java
+++ src/main/java/net/remind/Service/Answer.java
@@ -7,8 +7,11 @@ public class Answer {
 		if(content.equals("안녕")){
 			return "안녕하세욤";
 		}
-		if(content.equals("나어떤것같아")){
+		if(content.equals("나어떤것같아")||content.equals("해리")){
 			return "여신";
+		}
+		if(content.contains("솔비텍")){
+			return "퇴사";
 		}
 		return content;
 	}
