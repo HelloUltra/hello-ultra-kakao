0b8e76d6edbdd60e336ff81e248311f4c30b15d3
kwagdaeyong <dayong7387@hanmail.net>
2017-02-12 10:48:54 +0900

    modi

--- src/main/java/net/remind/Service/Answer.java
+++ src/main/java/net/remind/Service/Answer.java
@@ -1,24 +1,15 @@
 package net.remind.Service;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class Answer {
 
 	public String make_Message(String content) {
 		if(content.equals("안녕")){
 			return "안녕하세욤";
 		}
-		if(content.equals("나어떤것같아")||content.equals("해리")){
-			return "여신";
-		}
-		if(content.contains("솔비텍")){
-			return "퇴사";
-		}
-		if(content.equals("주먹")){
-			return "양기종,정현우,곽대용,박창우,형성관,박용옥";
-		}
-		if(content.contains("퇴사목록")){
-			return "곽대용,이은희,김준혁,나성훈,조윤식,설재희";
-		}
+		
 		return content;
 	}
 
--- src/test/java/net/remind/HelloUltraKakaoApplicationTests.java
+++ src/test/java/net/remind/HelloUltraKakaoApplicationTests.java
@@ -1,16 +1,26 @@
 package net.remind;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HelloUltraKakaoApplicationTests {
-
+	
+	@Autowired
+	TestRestTemplate testRestTemplate;
+	
 	@Test
 	public void contextLoads() {
+		Map test = testRestTemplate.getForObject("/message", HashMap.class);
+		
 	}
 
 }
