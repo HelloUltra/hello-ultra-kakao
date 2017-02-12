3b49e1783803f673dfc3b446bf1edf614c754df5
kwagdaeyong <dayong7387@hanmail.net>
2017-02-12 11:03:28 +0900

    tt

--- src/main/java/net/remind/Controller/MessageController.java
+++ src/main/java/net/remind/Controller/MessageController.java
@@ -8,9 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.remind.Model.Message;
 import net.remind.Service.Answer;
 
 @RestController
@@ -20,21 +20,23 @@ public class MessageController {
 	
 
 	@PostMapping("/message")
-	public Map<String, Object> message( @RequestBody Map<String,Object> getMessage) throws ParseException{
+	public Map<String, Object> message( @RequestBody Message message) throws ParseException{
 	
 		System.out.println("메세지받음");
 	
 		Map <String, Object> sendMessage = new HashMap<String, Object>();
 		Map <String, Object> sendObject = new HashMap<String, Object>();
-		log.debug("getMessage : {}", getMessage);
+		//log.debug("getMessage : {}", getMessage);
 		
-		String user_key = getMessage.get("user_key").toString();
-		String type = getMessage.get("type").toString();
-		String content = getMessage.get("content").toString();
+		//String user_key = getMessage.get("user_key").toString();
+		String user_key = message.getUser_key();
+		
+		//String type = getMessage.get("type").toString();
+		String type = message.getType();
+		//String content = getMessage.get("content").toString();
+		String content = message.getContent();
 		log.debug("user_key : {}",user_key);
-		
-		log.debug("type : {}",type);
-		
+		log.debug("type : {}",type);		
 		log.debug("content : {}",content);
 	
 		Answer answer = new Answer();
--- src/main/java/net/remind/Model/Message.java
+++ src/main/java/net/remind/Model/Message.java
@@ -5,4 +5,24 @@ public class Message {
 	String user_key;
 	String type;
 	String content;
+	public String getUser_key() {
+		return user_key;
+	}
+	public void setUser_key(String user_key) {
+		this.user_key = user_key;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 }
